732bc09803a61636942ab35734b5f2966f570a6f
Amit Ambardekar <amitamb@gmail.com>
2012-10-03 02:18:29 +0530

    Started with endless scroll project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Endlessscroll::Application.config.secret_token = 'bebaa9dc7086145730a378b8f990693bb1ef48dd62078e7c1c520c951740bf3e04729c2e0e1ea632de663e378376702298f9289566db788bf9a5adf3f722fe11'
